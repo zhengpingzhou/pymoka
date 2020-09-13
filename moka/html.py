613e6738ac3164948d7d22935260e0fba1b0e652
Zhengping Zhou <zpzhou@stanford.edu>
2020-09-12 18:35:01 -0700

    -

--- moka/html.py
+++ moka/html.py
@@ -230,6 +230,7 @@ class HTMLTable:
         if isinstance(value, str):
             filename = value.replace('/', '_')
             os.system(f'cp {value} {self.html.video_dir}/{filename}')
+            vd_abs_path = f'{self.html.video_dir}/{filename}'
             vd_rel_path = f'videos/{filename}'
 
         else:
@@ -242,8 +243,11 @@ class HTMLTable:
             save_video(vd_abs_path, value, fps=5, verbose=False)
             self.num_videos += 1
 
-        video(src=vd_rel_path, width=f'{width}px', height='auto', 
-              controls='true', autoplay='true', loop='true', muted='true', playsinline='true')
+        os.system(f'ffmpeg -y -loglevel error -i {vd_abs_path} -pix_fmt rgb24 {vd_abs_path.replace(".mp4", ".gif")}')
+        img(src=vd_rel_path.replace('.mp4', '.gif'), style=f"width:{width}px")
+
+        # video(src=vd_rel_path, width=f'{width}px', height='auto', 
+        #       controls='true', autoplay='true', loop='true', muted='true', playsinline='true')
 
 
     def set_header(self, *args, **kwargs):
